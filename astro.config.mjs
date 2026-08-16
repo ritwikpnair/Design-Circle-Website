59a25e7792e8a32f8cc1626d3f38f3f29a4a8cac
Ritwik P. Nair <ritwikpnair@proton.me>
2026-08-16 16:07:34 +0530

    Added a contact page and made some adjustments to the header, footer, and blog index page. Updated global styles for better layout and responsiveness.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 	fonts: [
 		{
 			provider: fontProviders.local(),
